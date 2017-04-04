15fa83bc91709767fcc5a43f45bad4b4c6f10b4b
Jake Siebers <jakesiebers@Jakes-MacBook-Pro.local>
2017-04-04 10:38:17 -0700

    Arrow functions vs regular functions

---
+++ functionvsarrowfunction.js
@@ -0,0 +1,32 @@
+
+// Function vs Arrow function
+
+// see closure
+const printContextExample = () => {
+
+  function printContext(){
+    console.log(this);
+    console.log(arguments);
+  }
+
+  printContext(1, 2, 3);
+  printContext.call({ key: 'value' }, 1, 2, 3);
+
+}
+
+// see references
+const arrowFunctionExample = () => {
+
+  function keyPrinterMaker(){
+    return () => console.log(this.key);
+  }
+
+  const a = {
+    key: 'value'
+  };
+  const printKey = keyPrinterMaker.call(a);
+  printKey();
+  a.key = 'value2';
+  printKey();
+
+}
