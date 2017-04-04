c3a77669e45bb49a3d54431a9b3b1cbc87ce9162
Jake Siebers <jakesiebers@Jakes-MacBook-Pro.local>
2017-04-04 10:37:00 -0700

    overview

---
+++ overview.js
@@ -0,0 +1,27 @@
+
+const / var / let
+
+references
+
+New and the prototype chain
+
+Scope/Closure
+
+Function vs Arrow function
+
+Promises
+
+map / reduce / filter
+
+ES6 Classes
+
+Currying
+
+Destructuring Bind / Default Parameter
+
+Template Literals
+
+
+---------------------------------------------------------------
+
+Generators
