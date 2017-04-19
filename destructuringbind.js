39382d1aaef17390d589d2c527f88ebc1352f8d0
Jake Siebers <jakesiebers@gmail.com>
2017-04-19 14:37:45 -0700

    Destructure bind more info

--- destructuringbind.js
+++ destructuringbind.js
@@ -2,3 +2,7 @@
 // Destructuring bind
 
 const getZoneIdFromAWSResponse = ({ HostedZones: [ zone ] }) => zone.Id;
+
+const data = { eventId: 'asdfasf', something: 5 };
+const { eventId, something } = data;
+console.log(eventId, something);
